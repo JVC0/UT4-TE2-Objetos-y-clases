62b4e63ef0eff2f9415f3a48504b5d6254d5aff4
José Manuel Peña Rodríguez <josepenrod@gmail.com>
2024-05-18 14:01:38 +0100

    Hecho la parte de clase Hand y Card

--- cards.py
+++ cards.py
@@ -1,10 +1,31 @@
 class Card:
-    #[2,3,4,5,6,7,8,9,10,J,Q,K,A] Posible atributo de clase 
-    def __init__(self, card_values:str) -> None:
-        self.rank, self.suit=card_values
+    #[2,3,4,5,6,7,8,9,10,J,Q,K,A] Posible atributo de clase
+    def __init__(self, card_values: str) -> None:
+        self.rank, self.suit = list(card_values)
         #self.value= Posible atributo de prueba
+
 
 class Hand:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, cat: list) -> None:
+        self.cat = ('HIGH_CARD', 'ONE_PAIR', 'TWO_PAIR', 'THREE_OF_A_KIND',
+                    'STRAIGHT', 'FLUSH', 'FULL_HOUSE', 'FOUR_OF_A_KIND',
+                    'STRAIGHT_FLUSH')
+
+    # def HIGH_CARD(self):
+
+    # def ONE_PAIR(self):
+
+    # def TWO_PAIR(self):
+
+    # def THREE_OF_A_KIND(self):
+
+    # def STRAIGHT(self):
+
+    # def FLUSH(self):
+
+    # def FULL_HOUSE(self):
+
+    # def FOUR_OF_A_KIND(self):
+
+    # def STRAIGHT_FLUSH(self):
