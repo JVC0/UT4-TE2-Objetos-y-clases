893dc0aeffb16dc23bbc553e8e7368f692219926
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-05-31 18:36:21 +0100

    Subida de game a proyecto joseph

--- Proyecto Joseph/game.py
+++ Proyecto Joseph/game.py
@@ -1,2 +1,12 @@
+from cards import Card, Hand
+from roles import Player
+
 class Deck:
     def __init__():
+        pass
+
+def get_winner(
+    players: list[Player],
+    common_cards: list[Card],
+    private_cards: list[list[Card]],
+) -> tuple[Player | None, Hand]:
