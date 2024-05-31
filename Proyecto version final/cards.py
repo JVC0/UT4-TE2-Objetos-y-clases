6ca0fc6c34ac465419dbe0e14810ce7ab6ab8e51
JVC0 <86218971+JVC0@users.noreply.github.com>
2024-05-31 18:04:20 +0100

    ..

--- Proyecto version final/cards.py
+++ Proyecto version final/cards.py
@@ -1,10 +1,23 @@
+from __future__ import annotations
+
 from helpers import combinations
+
 
 class Card:
     RANK_ORDER = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
     def __init__(self, card_values: str) -> None:
         self.rank, self.suit = list(card_values)
+
+    def __eq__(self, other: Card) -> bool:
+        return Card.RANK_ORDER.index(self.rank) == other.RANK_ORDER.index(other.rank)
+
+    def __lt__(self, other: Card) -> bool:
+        return Card.RANK_ORDER.index(self.rank) < other.RANK_ORDER.index(other.rank)
+
+    def __gt__(self, other: Card) -> bool:
+        return Card.RANK_ORDER.index(self.rank) > other.RANK_ORDER.index(other.rank)
+
 
 class Hand:
     HIGH_CARD = 0
@@ -17,8 +30,11 @@ class Hand:
     FOUR_OF_A_KIND = 7
     STRAIGHT_FLUSH = 8
 
-    def __init__(self, ) -> None:
-
+    def __init__(
+        self,
+    ) -> None:
+        pass
 
     def calc_best_hands(self):
         combinations()
+        pass
