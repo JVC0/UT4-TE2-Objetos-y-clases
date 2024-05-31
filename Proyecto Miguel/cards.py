42d9b03fd9228e589e881e9c88b50cbce2cfa2d6
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-05-31 17:02:05 +0100

    Subida de Rank Order

--- Proyecto Miguel/cards.py
+++ Proyecto Miguel/cards.py
@@ -1,31 +1,24 @@
+from helpers import combinations
+
 class Card:
-    #[2,3,4,5,6,7,8,9,10,J,Q,K,A] Posible atributo de clase
+    RANK_ORDER = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
     def __init__(self, card_values: str) -> None:
         self.rank, self.suit = list(card_values)
-        #self.value= Posible atributo de prueba
-
 
 class Hand:
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULL_HOUSE = 7
+    FOUR_OF_A_KIND = 8
+    STRAIGHT_FLUSH = 9
 
-    def __init__(self, cat: list) -> None:
-        self.cat = ('HIGH_CARD', 'ONE_PAIR', 'TWO_PAIR', 'THREE_OF_A_KIND',
-                    'STRAIGHT', 'FLUSH', 'FULL_HOUSE', 'FOUR_OF_A_KIND',
-                    'STRAIGHT_FLUSH')
+    def __init__(self, ) -> None:
 
-    # def HIGH_CARD(self):
 
-    # def ONE_PAIR(self):
-
-    # def TWO_PAIR(self):
-
-    # def THREE_OF_A_KIND(self):
-
-    # def STRAIGHT(self):
-
-    # def FLUSH(self):
-
-    # def FULL_HOUSE(self):
-
-    # def FOUR_OF_A_KIND(self):
-
-    # def STRAIGHT_FLUSH(self):
+    def calc_best_hands(self):
+        combinations()
