259df58d463195fe9bcc84ab5ac3708f865071db
JVC0 <86218971+JVC0@users.noreply.github.com>
2024-05-31 16:52:26 +0100

    SE HACE PARTE DE LA CARD

--- Proyecto Joseph/cards.py
+++ Proyecto Joseph/cards.py
@@ -6,7 +6,15 @@ class Card:
 
 
 class Hand:
-
+    HIGH_CARD=1
+    ONE_PAIR=2
+    TWO_PAIR=3
+    THREE_OF_A_KIND=4
+    STRAIGHT=5
+    FLUSH=6
+    FULL_HOUSE=7
+    FOUR_OF_A_KIND
+    STRAIGHT_FLUSH
     def __init__(self, cat: list) -> None:
         self.cat = ('HIGH_CARD', 'ONE_PAIR', 'TWO_PAIR', 'THREE_OF_A_KIND',
                     'STRAIGHT', 'FLUSH', 'FULL_HOUSE', 'FOUR_OF_A_KIND',
