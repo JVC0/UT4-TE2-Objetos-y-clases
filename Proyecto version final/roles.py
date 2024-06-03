cdda7933170762db8c773413465225e09f0e34c3
JVC0 <86218971+JVC0@users.noreply.github.com>
2024-06-03 19:11:30 +0100

    ..

---
+++ Proyecto version final/roles.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from cards import Card
+from game import Game
+
+class Dealer:
+    def __init__(self, common_cards: list[Card] | None = None) -> None:
+        self.game_instance = Game()
+        self.deck_generator = self.game_instance.deck_cards()
+        if common_cards is None:
+            self.common_cards = [
+                Card(next(self.deck_generator)),
+                Card(next(self.deck_generator)),
+                Card(next(self.deck_generator)),
+                Card(next(self.deck_generator)),
+                Card(next(self.deck_generator)),
+            ]
+        else:
+            self.common_cards = common_cards
+
+class Player:
+    def __init__(self, name: str, dealer: Dealer, private_cards: list[Card] | None = None):
+        self.name = name
+        if private_cards is None:
+            self.private_cards = [
+                Card(next(dealer.deck_generator)),
+                Card(next(dealer.deck_generator))
+            ]
+        else:
+            self.private_cards = private_cards
