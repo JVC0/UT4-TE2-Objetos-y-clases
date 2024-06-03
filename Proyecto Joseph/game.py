d9071fced5b53cc24c49a70d56b82d7bf13369a4
JVC0 <86218971+JVC0@users.noreply.github.com>
2024-06-03 16:27:32 +0100

    ...

--- Proyecto Joseph/game.py
+++ Proyecto Joseph/game.py
@@ -1,21 +1,36 @@
-from cards import Card, Hand
-from roles import Player,Dealer
-from helpers import shuffle
+from __future__ import annotations
 
-class game:
+from cards import Card, Hand
+from helpers import shuffle
+from roles import Dealer, Player
+
+
+class Game:
     def __init__(self) -> None:
-        pass
-    
-    def deck(): # puede mejorarse
-        deck = [
+        self.deck = []
+
+    def deck_cards(self):  # puede mejorarse
+        self.deck = [
             num + suit
             for suit in ['❤', '♠', '◆', '❤']
             for num in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         ]
-        shuffle(deck)
-        for card in deck:
+        shuffle(self.deck)
+        for card in self.deck:
             yield card
-    def get_winner(
-        players: list[Player],
-        common_cards: list[Card],
-        private_cards: list[list[Card]],) -> tuple[Player | None, Hand]:
+
+    def __iter__(self) -> object:
+        return self
+
+    def __next__(self) -> Game:
+        pointer = 0
+
+        if pointer >= self.deck:
+
+            raise StopIteration
+
+        game = Game(str(pointer))
+
+        pointer += 1
+
+        return game
